b71a910e75b28c4c8a109dd15df34b088db3c6c0
shaobkj <shaobkj@users.noreply.github.com>
2026-01-25 19:25:28 +0800

    chore: bump version to 1.1.0 to ensure update detection

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
+# Version
+__version__ = "1.1.0"
+
 # Export WEB_DIRECTORY to allow ComfyUI to load the JS extension
 WEB_DIRECTORY = "js"
 
